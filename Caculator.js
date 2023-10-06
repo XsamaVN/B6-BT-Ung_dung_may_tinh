b30308142ae7c46e364998f9ad734cb81f6d04aa
XsamaVN <buianhduc97@gmail.com>
2023-10-06 22:05:26 +0700

    [Bài tập] Ứng dụng máy tính v2

--- Caculator.js
+++ Caculator.js
@@ -6,22 +6,34 @@ function cong() {
     ketqua =  Soa + Sob + "  (cộng)";
     document.getElementById("Ketqua").innerHTML = ketqua;
 }
-function tru() {    let Soa = +document.getElementById("soa").value;
+function tru() {
+    let Soa = +document.getElementById("soa").value;
     let Sob = +document.getElementById("sob").value;
     let ketqua;
     ketqua = Soa - Sob + "  (trừ)";
     document.getElementById("Ketqua").innerHTML = ketqua;
 }
-function nhan() {    let Soa = +document.getElementById("soa").value;
+function nhan() {
+    let Soa = +document.getElementById("soa").value;
     let Sob = +document.getElementById("sob").value;
     let ketqua;
     ketqua = Soa * Sob + "  (nhân)";
     document.getElementById("Ketqua").innerHTML = ketqua;
 }
-function chia() {    let Soa = +document.getElementById("soa").value;
+function chia() {
+    let Soa = +document.getElementById("soa").value;
     let Sob = +document.getElementById("sob").value;
     let ketqua;
     ketqua = Soa / Sob + "  (chia)";
     document.getElementById("Ketqua").innerHTML = ketqua;
 }
-
+// function pheptinh() {
+//     let Soa= +document.getElementById("soa").value;
+//     let Sob= +document.getElementById("sob").value;
+//     let ketqua;
+//     document.getElementById("Cong").innerHTML = Soa+Sob;
+//     document.getElementById("Tru").innerHTML = Soa-Sob;
+//     document.getElementById("Nhan").innerHTML = Soa*Sob;
+//     document.getElementById("Chia").innerHTML = Soa/Sob;
+//
+// }
